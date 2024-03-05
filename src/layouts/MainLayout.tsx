3c7fba71e951219d708f306e5bdee4e52993f667
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-03-05 23:27:44 +0300

    feat: redo main layout to ant design

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,31 +1,59 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import "./MainLayout.scss";
+import { Layout, Menu } from "antd";
+import type { MenuProps } from "antd";
+
+const { Header, Content } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    label: <NavLink to="/">Home</NavLink>,
+    key: "home",
+  },
+  {
+    label: <NavLink to="/episodes">Episodes</NavLink>,
+    key: "episodes",
+  },
+  {
+    label: <NavLink to="/locations">Locations</NavLink>,
+    key: "locations",
+  },
+  {
+    label: <NavLink to="/characters">Characters</NavLink>,
+    key: "characters",
+  },
+  {
+    label: <NavLink to="/plumbus">Plumbus</NavLink>,
+    key: "plumbus",
+  },
+];
 
 const MainLayout = () => {
   return (
-    <>
-      <nav>
-        <ul className="nav-list">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/episodes">Episodes</NavLink>
-          </li>
-          <li>
-            <NavLink to="/locations">Locations</NavLink>
-          </li>
-          <li>
-            <NavLink to="/characters">Characters</NavLink>
-          </li>
-          <li>
-            <NavLink to="plumbus">Plumbus</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
-    </>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items}
+        />
+      </Header>
+      <Content style={{ padding: "0 48px" }}>
+        <Outlet />
+      </Content>
+    </Layout>
   );
 };
 
