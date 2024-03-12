146acf1fc6d80b9d5d83542715ad3bc95a054aa2
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-03-12 22:49:19 +0300

    feat: add sorting to characters list

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -2,16 +2,44 @@ import React, { useState } from "react";
 import characters from "../mock/characters.json";
 import { CharacterData } from "../components/Character";
 import CharactersList from "../components/CharactersList";
-import { Card, Typography } from "antd";
+import { Card, Flex, Form, Select, Typography } from "antd";
+import { compareByCreationDate } from "./Episodes";
+import { OrderDirection, useArrayOrder } from "../hooks/useArrayOrder";
 
 const { Title } = Typography;
 
 const Characters = () => {
   const [charactersArray] = useState<CharacterData[]>(characters ?? []);
+  const { array: sortedArray, toggleOrder } = useArrayOrder(
+    charactersArray,
+    compareByCreationDate,
+  );
+  const handleChangeSortDirection = (value: OrderDirection) => {
+    toggleOrder(value ?? "");
+  };
   return (
     <>
-      <Card title={<Title style={{ margin: 0 }}>Characters</Title>}>
-        <CharactersList items={charactersArray} style={{ overflow: "auto" }} />
+      <Card
+        title={
+          <Flex
+            style={{ justifyContent: "space-between", alignItems: "center" }}
+          >
+            <Title style={{ margin: 0 }}>Characters</Title>
+            <Form.Item label="Sort by creation date" style={{ margin: 0 }}>
+              <Select
+                style={{ width: 120 }}
+                allowClear
+                options={[
+                  { value: "asc", label: "Ascending" },
+                  { value: "desc", label: "Descending" },
+                ]}
+                onChange={handleChangeSortDirection}
+              />
+            </Form.Item>
+          </Flex>
+        }
+      >
+        <CharactersList items={sortedArray} style={{ overflow: "auto" }} />
       </Card>
     </>
   );
