bb7229d216f9b768ef67a4fceddf4b7dc4dc4f07
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-03-30 17:21:01 +0300

    refactor: cleanup login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,33 +11,16 @@ const { Item } = Form;
 const { Password } = Input;
 
 const Login = () => {
-  const { user, login, logout } = useAuth();
+  const { login } = useAuth();
   const location = useLocation();
   const from = location.state?.from || "/";
   const navigate = useNavigate();
-  // TODO: remove console.logs
   const handleFinish: FormProps<Required<FieldType>>["onFinish"] = (values) => {
-    console.log(values);
     login(values, () => navigate(from, { replace: true }));
   };
-  const handleFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
-    errorInfo,
-  ) => {
-    console.log(errorInfo);
-  };
-  // TODO: remove auth user info
   return (
     <Card style={{ maxWidth: "700px", margin: "auto" }}>
-      <p>{user?.name || "not loggen in"}</p>
-      {!!user && (
-        <Button onClick={() => logout(() => navigate("/"))}>logout</Button>
-      )}
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        onFinish={handleFinish}
-        onFinishFailed={handleFinishFailed}
-      >
+      <Form name="basic" labelCol={{ span: 8 }} onFinish={handleFinish}>
         <Item
           label="Username"
           name="username"
